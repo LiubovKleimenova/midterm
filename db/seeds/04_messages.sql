711041b36519ca4da50d21c91af174b1070136ab
Steven Choi <stevencschoi87@gmail.com>
2020-03-07 19:10:48 +0000

    added databases for messages and dummy data

---
+++ db/seeds/04_messages.sql
@@ -0,0 +1,11 @@
+-- Widgets table seeds here (Example)
+INSERT INTO messages (receiver_id, cat_id, sender_id, message)
+VALUES (2, 3, 1, 'Hi, I am interested in adopting Chester & Rusty - are they still available??');
+INSERT INTO messages (receiver_id, cat_id, sender_id, message)
+VALUES (2, 4, 1, 'I love jaguars!! Can you give me a break on the fees though?');
+INSERT INTO messages (receiver_id, cat_id, sender_id, message)
+VALUES (3, 5, 1, 'Omg he is sooo cute! I must have him!');
+INSERT INTO messages (receiver_id, cat_id, sender_id, message)
+VALUES (3, 9, 1, 'Is Berk up to date with his immunizations?');
+INSERT INTO messages (receiver_id, cat_id, sender_id, message)
+VALUES (3, 3, 1, 'I am seriously interested in Baboo - can we arrange a meet up?');
