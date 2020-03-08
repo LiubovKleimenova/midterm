be155eead6e2bad832e73e6cd307e4a097b08050
Steven Choi <50030216+stevencschoi@users.noreply.github.com>
2020-03-07 19:02:37 -0500

    refactored email function into database.js routing in admin.js (#6)

--- routes/database.js
+++ routes/database.js
@@ -1,20 +1,16 @@
 const nodemailer = require('nodemailer');
 module.exports = (db) => {
-
 // *********** HELPER FUNCTIONS FOR USER ROUTES ************
-
   const getAllCats = function () {
     return db.query(`SELECT * FROM cats;`)
     .then(res => res.rows)
     // .catch(err => console.error('query error', err.stack));
   }
-
   const getAllUsers = function () {
     return db.query(`SELECT * FROM users;`)
     .then(res => res.rows[0] )
     // .catch(err => console.error('query error', err.stack));
   }
-
   const getFavourites = function () {
     return db.query(`
     SELECT * FROM cats
@@ -22,7 +18,6 @@ module.exports = (db) => {
     WHERE favourites.user_id = 2;`)
     .then(res => res.rows )
   }
-
   const filterBySearch = function(options) {
     const queryParams = [];
     const whereClauses = [];
@@ -30,7 +25,6 @@ module.exports = (db) => {
     SELECT *
     FROM cats
     `;
-
   if (options.minimum_fee && options.maximum_fee) {
     queryParams.push(`${options.minimum_fee}`);
     queryParams.push(`${options.options.maximum_fee}`);
@@ -40,41 +34,32 @@ module.exports = (db) => {
     queryParams.push(`%${options.region}%`);
     whereClauses.push(`region = $${queryParams.length} `);
   }
-
   if (options.size) {
     queryParams.push(options.size);
     whereClauses.push(`size = $${queryParams.length} `);
   }
-
   if (options.species) {
     queryParams.push(`${options.species}`);
     whereClauses.push(`species >= $${queryParams.length}`);
   }
-
   if (whereClauses.length) {
     queryString += `WHERE ${whereClauses.join(' AND ')}`;
   }
-
     queryString += `
     ORDER BY fee
     LIMIT 10;
     `;
-
     // 6
     return db.query(queryString, queryParams)
     .then(res => res.rows);
   }
-
-
 // *********** HELPER FUNCTIONS FOR ADMIN ROUTES ************
-
   const getMyCats = function () {
     return db.query(`
     SELECT * FROM cats
     WHERE owner_id = 2;`)
     .then(res => res.rows )
 }
-
 const getMessages = function () {
   return db.query(`
   SELECT * FROM cats
@@ -83,7 +68,7 @@ const getMessages = function () {
 }
 // email sending function
 // async..await is not allowed in global scope, must use a wrapper
-async function sendEmail() {
+async function sendEmail(to, subject, text) {
   // Generate test SMTP service account from ethereal.email
   // create reusable transporter object for host configuration
   let transporter = nodemailer.createTransport({
@@ -95,20 +80,17 @@ async function sendEmail() {
       pass: '' // generated ethereal password
     }
   });
-
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: '"Meowzza 👻" <stevencschoi87@gmail.com>', // sender address
-    to: "stevenspamlol@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<p>This is a test!</p>" // html body
+    to: `${to}`, // list of receivers
+    subject: `${subject}`, // Subject line
+    text: `${text}` // plain text body
+    // html: "<p>This is a test!</p>" // html body
   });
-
   console.log("Message sent: %s", info.messageId);
   // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 }
-
   return {getAllCats, getAllUsers, getFavourites, filterBySearch, getMessages, getMyCats, sendEmail};
 };
