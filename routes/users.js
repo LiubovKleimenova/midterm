240e2e521eab7be31e2d583aba6313205033f256
Ata Ansari <55994993+AtaAnsari@users.noreply.github.com>
2020-03-09 14:34:48 -0400

    refactored routes to use the userid for a logged in user (#14)

--- routes/users.js
+++ routes/users.js
@@ -22,7 +22,8 @@ module.exports = (databaseHelperFunctions) => {
   // Only favourite cats are displayed when the 'show favourites' button is clicked
   router.get("/favourites", (req, res) => {
     console.log('IT WORKS');
-    databaseHelperFunctions.getFavouritesUser()
+    userid =  req.session.userId
+    databaseHelperFunctions.getFavourites(userid)
     .then(data => res.json(data))
     .catch(err => res.status(500).send(err))
   });
