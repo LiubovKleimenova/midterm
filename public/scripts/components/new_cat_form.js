be74ae62a6dc9f3afdf5ab62c09a35f3119abf1c
Steven Choi <stevencschoi87@gmail.com>
2020-03-11 18:38:10 +0000

    added styles to add cat form

--- public/scripts/components/new_cat_form.js
+++ public/scripts/components/new_cat_form.js
@@ -8,8 +8,8 @@ function addNewCatForm(user) {
         <input type="text" name="cat_name" placeholder="Fluffy" id="new-cat-form__name">
       </fieldset>
 
-      <fieldset class="new-cat-form__field-wrapper">
-        <textarea placeholder="Add your description here" name="description" id="cat-form__description" cols="50" rows="10"></textarea>
+      <fieldset>
+        <textarea placeholder="Add your description here" name="description" id="cat-form__description" cols="30" rows="4"></textarea>
       </fieldset>
 
       <div class="new-cat-form__field-wrapper">
@@ -21,7 +21,7 @@ function addNewCatForm(user) {
         <input placeholder="Puma" type="text" name="species" id="new-cat-form__species">
       </div>
       <div class="new-cat-form__field-wrapper">
-        <label for="new-cat-form__size">Size</label>
+        <label for="new-cat-form__size">Size:</label>
         <select id="new-cat-form__size" name="size">
           <option name="size" value="s">S</option>
           <option name="size" value="m">M</option>
@@ -46,19 +46,20 @@ function addNewCatForm(user) {
       </div>
 
       <div class="new-cat-form__field-wrapper">
-        <label for="new-cat-form__cover">Cat's Image</label>
-        <input placeholder="Main Image" type="text" name="cover_photo_url" id="new-cat-form__cover">
+        <label for="new-cat-form__cover">Cat's Image URL</label>
+        <input placeholder="Image URL" type="text" name="cover_photo_url" id="new-cat-form__cover">
       </div>
-
-      <div class="new-cat-form__field-wrapper">
-        <button class="mdc-button mdc-button--raised" type="submit">
-          <div class="mdc-button__ripple"></div>
-          Create
-        </button>
-        <button class="mdc-button mdc-button--raised" type="reset">
-          <div class="mdc-button__ripple"></div>
-          Reset
-        </button>
+      <div class="form-buttons">
+        <div class="new-cat-form__field-wrapper">
+          <button class="mdc-button mdc-button--raised" type="submit">
+            <div class="mdc-button__ripple"></div>
+            Create
+          </button>
+          <button class="mdc-button mdc-button--raised" type="reset">
+            <div class="mdc-button__ripple"></div>
+            Reset
+          </button>
+        </div>
       </div>
 
     </form>`;
