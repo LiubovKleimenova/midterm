864378720c1870b1dc5dab6cc44603047756ef64
Steven Choi <stevencschoi87@gmail.com>
2020-03-11 18:02:07 +0000

    adding styles to add cat form

--- public/scripts/components/new_cat_form.js
+++ public/scripts/components/new_cat_form.js
@@ -2,9 +2,9 @@ function addNewCatForm(user) {
   const $catFormSection = $(".new-cat-form-section");
   const $newListing = $(".new-listing");
   let $newCatForm = `<form action="/admin/newcat" method="POST" id="new-cat-form" class="new-cat-form">
-      <p>Create your cat listing</p>
+      <h2><i class="fas fa-paw"></i> Create your cat listing</h2>
       <fieldset class="new-cat-form__field-wrapper">
-        <label for="new-name-form__name">Cat name</label>
+        <label for="new-name-form__name">Your cat's name:</label>
         <input type="text" name="cat_name" placeholder="Fluffy" id="new-cat-form__name">
       </fieldset>
 
@@ -13,12 +13,14 @@ function addNewCatForm(user) {
       </fieldset>
 
       <div class="new-cat-form__field-wrapper">
-        <label for="new-cat-form__birth-date">Date of birth</label>
+        <label for="new-cat-form__birth-date">Date of birth:</label>
         <input placeholder="2018-12-31" type="date" name="birth-date" id="new-cat-form__birth-date">
-
-        <label for="new-cat-form__species">Species</label>
+      </div>
+      <div class="new-cat-form__field-wrapper">
+        <label for="new-cat-form__species">Species:</label>
         <input placeholder="Puma" type="text" name="species" id="new-cat-form__species">
-
+      </div>
+      <div class="new-cat-form__field-wrapper">
         <label for="new-cat-form__size">Size</label>
         <select id="new-cat-form__size" name="size">
           <option name="size" value="s">S</option>
@@ -28,7 +30,7 @@ function addNewCatForm(user) {
       </div>
 
       <div class="new-cat-form__field-wrapper">
-        <label for="new-cat-form__region">Region</label>
+        <label for="new-cat-form__region">Region:</label>
         <select id="new-cat-form__region" name="region">
           <option name="region" value="asia">Asia</option>
           <option name="region" value="europe">Europe</option>
@@ -39,7 +41,7 @@ function addNewCatForm(user) {
       </div>
 
       <div class="new-cat-form__field-wrapper">
-        <label for="new-cat-form__fee">Adoption fee</label>
+        <label for="new-cat-form__fee">Adoption fee:</label>
         <input placeholder="5000" type="number" name="adoption_fee" id="new-cat-form__fee">
       </div>
 
