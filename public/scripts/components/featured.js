b9c0999a20de83a680207e397eea0c8adcc6a5ed
Steven Choi <50030216+stevencschoi@users.noreply.github.com>
2020-03-12 12:43:19 -0400

    added logic to append carousel with featured cats (#48)

--- public/scripts/components/featured.js
+++ public/scripts/components/featured.js
@@ -1,6 +1,5 @@
 window.Meowza.createFeatured = cat => {
   if (cat.is_featured) {
-    // console.log('returning a cat!');
     return $(`
       <div class="carousel-cell">
         <img src="${cat.main_photo_url}" alt=${cat.name}>
