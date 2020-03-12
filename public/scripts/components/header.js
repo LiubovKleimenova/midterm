6d9a28329de5bfe596e404975824f3e96eb39a6d
Steven Choi <50030216+stevencschoi@users.noreply.github.com>
2020-03-11 22:26:12 -0400

    Features/styles2 (#42)
    * added incremental changes to padding/spacing on header
    
    * centered h2 on filter form and positioned elements in cat listing
    
    * revised variables in scss
    
    * adding styles to footer and buttons
    
    * added carousel to site
    
    * added pictures to carousel
    
    * add seeds to db
    
    * added styles to footer and buttons
    
    * updated stylings to filter form and create new cat form
    
    * adding styles to add cat form
    
    * added styles to add cat form
    
    * resolved conflicts
    
    * added stylings to add cat form and removed default stylings from buttons
    
    * added stylings to add cat form
    
    * line styling to form email line added
    
    * redefined messages section to inbox

--- public/scripts/components/header.js
+++ public/scripts/components/header.js
@@ -41,7 +41,7 @@ function updateHeader(user) {
     } else {
       if (!user.is_admin) {
         usersLinks = `
-          <li class="padding-left padding-right message-link">My Messages</li>
+          <li class="hover padding-left padding-right message-link">Inbox</li>
           <li class="padding-left padding-right">Hi, ${user.name}!</li>
           <button class="logout-button mdc-button mdc-button--raised">
             <div class="mdc-button__ripple"></div>
@@ -50,7 +50,7 @@ function updateHeader(user) {
         `;
       } else {
         usersLinks = `
-          <li class="padding-left padding-right message-link">My Messages</li>
+          <li class="padding-left padding-right message-link">Inbox</li>
           <li class="hover user-link owner-button">
             <a>My cats</a>
           </li>
